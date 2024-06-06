5790ddbb231a627a19180598be08f589af8e94a5
LaserCrafter <elvisbui36@gmail.com>
2024-06-05 17:09:57 -0700

    Imported and Deployed React app to Github Pages with MUI Styling.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Button, TextField, Container, Typography, AppBar, Toolbar, IconButton, Paper } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import Box from '@mui/material/Box'; // Ensure Box is imported
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    <Container>
+      <Box className="large-container">
+        <Typography variant="h2" gutterBottom>
+          Hello, I'm Elvis Bui.
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          I am an aspiring software developer currently looking to work with open minds and good ideas
+        </Typography>
+        <Container>
+      <Box className="social-media-container">
+        <IconButton color="primary" href="https://www.linkedin.com/in/elvisbui" target="_blank" rel="noopener noreferrer">
+          <LinkedInIcon />
+        </IconButton>
+        <IconButton color="primary" href="https://github.com/LaserCraftet" target="_blank" rel="noopener noreferrer">
+          <GitHubIcon />
+        </IconButton>
+      </Box>
+    </Container>
+      </Box>
+    </Container>
     </div>
   );
 }
