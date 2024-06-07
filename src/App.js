a3ecb498b66e0936bc66c40a33e4cd10ee9f177d
LaserCrafter <elvisbui36@gmail.com>
2024-06-06 18:05:58 -0700

    Created routing for other pages along with clickable menu bars and pages

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import Box from '@mui/material/Box'; // Ensure Box is imported
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import './App.css';
-import MenuBar from './MenuBar.js'
+import { Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+import MenuBar from './MenuBar';
 
-function App() {
+function Main() {
   return (
     <div>
-      <AppBar position="static">
-      <MenuBar />
-      </AppBar>
     <Container>
       <Box className="large-container">
         <Typography variant="h2" gutterBottom>
@@ -25,13 +26,32 @@ function App() {
         <IconButton color="primary" href="https://www.linkedin.com/in/elvisbui" target="_blank" rel="noopener noreferrer">
           <LinkedInIcon />
         </IconButton>
-        <IconButton color="primary" href="https://github.com/LaserCraftet" target="_blank" rel="noopener noreferrer">
+        <IconButton color="primary" href="https://github.com/LaserCrafter" target="_blank" rel="noopener noreferrer">
           <GitHubIcon />
         </IconButton>
       </Box>
     </Container>
       </Box>
     </Container>
+    </div>
+  );
+}
+
+
+function App() {
+  return (
+    <div>
+      <AppBar position="static">
+      <MenuBar />
+      </AppBar>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Container>      
     </div>
   );
 }
--- src/MenuBar.js
+++ src/MenuBar.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import Typography from '@mui/material/Typography';
 import MenuIcon from '@mui/icons-material/Menu';
 
 function MenuBar() {
@@ -23,15 +25,18 @@ function MenuBar() {
         <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleMenuOpen}>
           <MenuIcon />
         </IconButton>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          My App
+        </Typography>
         <Menu
           anchorEl={anchorEl}
           open={Boolean(anchorEl)}
           onClose={handleMenuClose}
         >
-          <MenuItem onClick={handleMenuClose}>Home</MenuItem>
-          <MenuItem onClick={handleMenuClose}>About</MenuItem>
-          <MenuItem onClick={handleMenuClose}>Projects</MenuItem>
-          <MenuItem onClick={handleMenuClose}>Contact</MenuItem>
+          <MenuItem onClick={handleMenuClose} component={Link} to="/">Home</MenuItem>
+          <MenuItem onClick={handleMenuClose} component={Link} to="/about">About</MenuItem>
+          <MenuItem onClick={handleMenuClose} component={Link} to="/projects">Projects</MenuItem>
+          <MenuItem onClick={handleMenuClose} component={Link} to="/contact">Contact</MenuItem>
         </Menu>
       </Toolbar>
     </AppBar>
@@ -39,3 +44,4 @@ function MenuBar() {
 }
 
 export default MenuBar;
+
